4aaead56b4cc3225314d1b51746b6c22c1ff4abf
broccolai <me@broccol.ai>
2023-11-08 23:52:33 +0000

    feature: add ability to import from cubito FlightFeathers

--- src/main/java/love/broccolai/beanstalk/commands/command/BeanstalkAdminCommand.java
+++ src/main/java/love/broccolai/beanstalk/commands/command/BeanstalkAdminCommand.java
@@ -7,30 +7,44 @@ import cloud.commandframework.arguments.standard.EnumArgument;
 import cloud.commandframework.bukkit.parsers.PlayerArgument;
 import cloud.commandframework.context.CommandContext;
 import com.google.inject.Inject;
+import java.io.File;
 import java.time.Duration;
+import java.util.UUID;
 import love.broccolai.beanstalk.commands.cloud.CloudArgumentFactory;
 import love.broccolai.beanstalk.model.profile.Profile;
 import love.broccolai.beanstalk.service.item.ItemService;
 import love.broccolai.beanstalk.service.message.MessageService;
+import love.broccolai.beanstalk.service.profile.ProfileService;
+import love.broccolai.beanstalk.service.profile.provider.ProfileCacheProvider;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 public final class BeanstalkAdminCommand implements PluginCommand {
 
     private final CloudArgumentFactory argumentFactory;
     private final MessageService messageService;
+    private final ProfileService profileService;
     private final ItemService itemService;
+
+    private final ProfileCacheProvider profileCacheProvider;
 
     @Inject
     public BeanstalkAdminCommand(
         final CloudArgumentFactory argumentFactory,
         final MessageService messageService,
-        final ItemService service
+        final ProfileService profileService,
+        final ItemService service,
+        final ProfileCacheProvider profileCacheProvider
     ) {
         this.argumentFactory = argumentFactory;
         this.messageService = messageService;
+        this.profileService = profileService;
         this.itemService = service;
+        this.profileCacheProvider = profileCacheProvider;
     }
 
     @Override
@@ -57,6 +71,12 @@ public final class BeanstalkAdminCommand implements PluginCommand {
             .argument(this.argumentFactory.profile("target", true))
             .argument(DurationArgument.of("duration"))
             .handler(this::handleModify)
+        );
+
+        commandManager.command(baseCommand
+            .literal("import")
+            .literal("cubito")
+            .handler(this::handleCubitoImport)
         );
     }
 
@@ -96,6 +116,38 @@ public final class BeanstalkAdminCommand implements PluginCommand {
         this.stopFlyingIfDurationZero(target);
 
         this.messageService.modifyTarget(sender, target, target.flightRemaining());
+    }
+
+    //todo: hide this away somewhere?
+    private void handleCubitoImport(final CommandContext<CommandSender> context) {
+        Player sender = (Player) context.getSender();
+
+        File folder = new File(Bukkit.getPluginsFolder(), "FlightFeathers");
+        File file = new File(folder, "config.yml");
+
+        if (!file.exists()) {
+            sender.sendMessage("cubito config.yml does not exist!");
+            return;
+        }
+
+        FileConfiguration configuration = YamlConfiguration.loadConfiguration(file);
+        ConfigurationSection flightTimeSection = configuration.getConfigurationSection("FlightFeathers.FlyingTime");
+
+        if (flightTimeSection == null) {
+            sender.sendMessage("could not load cubito config.yml!");
+            return;
+        }
+
+        for (String serializedId : flightTimeSection.getKeys(false)) {
+            UUID uuid = UUID.fromString(serializedId);
+            Duration duration = Duration.ofSeconds(configuration.getInt(serializedId));
+
+            this.profileService.get(uuid).flightRemaining(current -> current.plus(duration));
+            sender.sendMessage("imported " + duration.toSeconds() + "s for " + uuid);
+        }
+
+        this.profileCacheProvider.close();
+        sender.sendMessage("successfully imported from cubito!");
     }
 
     private void stopFlyingIfDurationZero(Profile profile) {
